8e89f9af7c272109b56984d0cc8a8144a6b5efd4
Maciej Leszczyński <dev@asista.pl>
2019-09-29 21:09:19 +0200

    Change age

--- src/pages/about.js
+++ src/pages/about.js
@@ -40,7 +40,7 @@ const AboutPage = () => (
         Hi! My name is Kasia
       </PageTitle>
       <Paragraph>
-        I'm a 23-year-old illustrator from Poland. I admire everything that's
+        I'm a 24-year-old illustrator from Poland. I admire everything that's
         cute and I try to express it in my art. I love to share joy so I'd be
         happy to illustrate (especially children's) books or anything related.
       </Paragraph>
@@ -55,7 +55,7 @@ const AboutPage = () => (
         }}
       >
         <Paragraph>
-          Cześć! Mam na imię Kasia i jestem 23-letnią ilustratorką z Polski.
+          Cześć! Mam na imię Kasia i jestem 24-letnią ilustratorką z Polski.
           Uwielbiam wszystko co urocze, co staram się wyrazić w swojej
           twórczości. Kocham szerzyć radość, więc z przyjemnością zilustruję
           książki (zwłaszcza dziecięce).
